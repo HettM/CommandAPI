899530301daae30ae76fea00f29ecfd77d0e0263
HettM <mhetterley@btinternet.com>
2020-10-20 16:11:54 +0100

    Full API Implemented

--- src/CommandAPI/Profiles/CommandsProfile.cs
+++ src/CommandAPI/Profiles/CommandsProfile.cs
@@ -8,6 +8,12 @@ namespace CommandAPI.Profiles
         public CommandsProfile()
         {
             CreateMap<Command, CommandReadDto>();
+
+            CreateMap<CommandCreateDto, Command>();
+
+            CreateMap<CommandUpdateDto, Command>();
+
+            CreateMap<Command, CommandUpdateDto>();
         }
     }
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -12,6 +12,7 @@ using CommandAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
 using AutoMapper;
+using Newtonsoft.Json.Serialization;
 
 namespace CommandAPI
 {
@@ -32,6 +33,11 @@ namespace CommandAPI
             builder.Password = Configuration["Password"];
 
             services.AddDbContext<CommandContext> (opt => opt.UseNpgsql(builder.ConnectionString));
+
+            services.AddControllers().AddNewtonsoftJson(s => 
+                {
+                    s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
 
             services.AddControllers();
 
